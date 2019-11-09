077310898f43048c266625a7cf679e00671a036a
albertqu <albert_qu@berkeley.edu>
2019-11-08 19:02:23 -0800

    added VBDE

---
+++ models.py
@@ -0,0 +1,41 @@
+from pyqlearning.qlearning.boltzmann_q_learning import BoltzmannQLearning
+from pyqlearning.qlearning.greedy_q_learning import GreedyQLearning
+import numpy as np
+
+class AdaptiveBetaQLearning(BoltzmannQLearning):
+
+    def __init__(self):
+        super(BoltzmannQLearning).__init__(self)
+        self.TDE = 0
+        self.sigma = 1
+
+    def __calculate_sigmoid(self):
+        """
+        SOFTMAX VBDE: f(s, a, \sigma) = \frac{2e^{-|\alpha TDE| / \sigma}}{1-e^{-|\alpha TDE|/\sigma}}
+        :return:
+        """
+        temp = np.exp(-self.alpha_value * np.abs(self.TDE) / self.sigma)
+        return 2*temp / (1-temp)
+
+
+
+    def update_q(self, state_key, action_key, reward_value, next_max_q):
+        '''
+        Update Q-Value.
+        Args:
+            state_key:              The key of state.
+            action_key:             The key of action.
+            reward_value:           R-Value(Reward).
+            next_max_q:             Maximum Q-Value.
+        '''
+        # Now Q-Value.
+        self.TDE = self.gamma_value * next_max_q - self.extract_q_df(state_key, action_key)
+        super(BoltzmannQLearning).update_q(state_key, action_key, reward_value, next_max_q)
+
+
+class AdaptiveEpsilonGreedy(GreedyQLearning):
+    pass
+
+
+
+
